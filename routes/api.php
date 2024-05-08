8e895c7cdd2eb9d20709195693fa4ce2f1875f97
arifhossen-dev <ahak.bsl@gmail.com>
2024-05-08 20:03:10 +0600

    Register Method create in AuthController

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login',[AuthController::class, 'login']);
+Route::post('/register',[AuthController::class, 'register']);
 
 Route::get('/tickets',function (){
     return Ticket::all();
