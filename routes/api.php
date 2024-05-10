8a1115a79db52c5de041c66571c6576e43eed3ae
arifhossen-dev <ahak.bsl@gmail.com>
2024-05-10 20:15:38 +0600

    Authentication login and logout api done

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,13 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\AuthController;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login',[AuthController::class, 'login']);
 Route::post('/register',[AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::get('/tickets',function (){
     return Ticket::all();
