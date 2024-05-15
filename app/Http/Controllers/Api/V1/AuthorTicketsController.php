52e2b37c5c91a79c40e263d73c6c0a995c61978c
arifhossen-dev <ahak.bsl@gmail.com>
2024-05-15 12:07:40 +0600

    Creating Resources with Post Requests

--- app/Http/Controllers/Api/V1/AuthorTicketsController.php
+++ app/Http/Controllers/Api/V1/AuthorTicketsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Filters\V1\TicketFilter;
+use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
 
@@ -13,4 +14,20 @@ class AuthorTicketsController extends Controller
     {
         return TicketResource::collection(Ticket::whereUserId($authorId)->filter($filters)->paginate());
     }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store($authorId, StoreTicketRequest $request)
+    {
+        $model = [
+            'title' => $request->input('data.attributes.title'),
+            'description' => $request->input('data.attributes.description'),
+            'status' => $request->input('data.attributes.status'),
+            'user_id' => $authorId,
+        ];
+
+        return new TicketResource(Ticket::create($model));
+    }
+
 }
