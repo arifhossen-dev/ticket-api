4f6777c58271a8e756b438f62cc8a2c90ad79717
arifhossen-dev <ahak.bsl@gmail.com>
2024-05-26 19:47:41 +0600

    Ticket Replace operation done

--- app/Http/Controllers/Api/V1/AuthorTicketsController.php
+++ app/Http/Controllers/Api/V1/AuthorTicketsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Filters\V1\TicketFilter;
+use App\Http\Requests\Api\V1\ReplaceTicketRequest;
 use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
@@ -32,6 +33,29 @@ class AuthorTicketsController extends Controller
         ];
 
         return new TicketResource(Ticket::create($model));
+    }
+
+    public function replace(ReplaceTicketRequest $request, $authorId, $ticketId)
+    {
+        // PUT
+
+        try {
+            $ticket = Ticket::findOrFail($ticketId);
+            if ($ticket->user_id == $authorId) {
+                $model = [
+                    'title' => $request->input('data.attributes.title'),
+                    'description' => $request->input('data.attributes.description'),
+                    'status' => $request->input('data.attributes.status'),
+                    'user_id' => $request->input('data.relationships.author.data.id')
+                ];
+
+                $ticket->update($model);
+                return new TicketResource($ticket);
+            }
+            // Todo: ticket doesn't belong to this user
+        } catch (ModelNotFoundException $exception) {
+            return $this->error('Ticket cannot be found.', 404);
+        }
     }
 
 
