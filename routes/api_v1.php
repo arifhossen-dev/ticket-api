9aa9116ce4572ed7a53260568a7c48c4b3b2dd38
arifhossen-dev <ahak.bsl@gmail.com>
2024-05-26 19:47:05 +0600

    Auth::sanctum group implement and replace PUT route create

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -6,11 +6,15 @@ use App\Http\Controllers\Api\V1\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('tickets', TicketController::class)->except('update');
+    Route::put('tickets/{ticket}', [TicketController::class, 'replace']);
 
-Route::middleware('auth:sanctum')->apiResource('/tickets', TicketController::class);
-Route::middleware('auth:sanctum')->apiResource('/authors', AuthorController::class);
-Route::middleware('auth:sanctum')->apiResource('/authors.tickets', AuthorTicketsController::class);
+    Route::apiResource('authors', AuthorController::class);
+    Route::apiResource('authors.tickets', AuthorTicketsController::class)->except('update');
+    Route::put('/authors/{author}/tickets/{ticket}', [AuthorTicketsController::class, 'replace']);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
